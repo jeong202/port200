642d826f86a74c8084a6354bbc46c441290bbbb4
jeong202 <jhyjhy968@gmail.com>
2023-12-19 14:24:28 +0900

    20231219/오후2시
    20231219/오후2시

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,9 @@
-import { intro } from "./intro.js";
-import { port } from "./port.js";
-import { about } from "./about.js"
-import { tech } from "./tech.js"
-import { info } from "./info.js"
+
 import { contact } from "./contact.js";
 import { smooth } from "./smooth.js";
 
 window.addEventListener("load", function () {
-  intro();
-  port();
-  about();
-  tech();
-  info();
+
   contact();
   smooth();
 });
